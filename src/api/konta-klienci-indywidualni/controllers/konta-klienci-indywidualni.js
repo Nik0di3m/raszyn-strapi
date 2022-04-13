159e9a5bf9205624e424c1d8eabff196399f0c25
Nik0di3m <norbert.waleszczyk@icloud.com>
2022-04-13 10:01:02 +0200

    find by slug

--- src/api/konta-klienci-indywidualni/controllers/konta-klienci-indywidualni.js
+++ src/api/konta-klienci-indywidualni/controllers/konta-klienci-indywidualni.js
@@ -1,9 +1,25 @@
-'use strict';
+"use strict";
 
 /**
  *  konta-klienci-indywidualni controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::konta-klienci-indywidualni.konta-klienci-indywidualni');
+module.exports = createCoreController(
+  "api::konta-klienci-indywidualni.konta-klienci-indywidualni",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id: slug } = ctx.params;
+      const { query } = ctx;
+      if (!query.filters) query.filters = {};
+      query.filters.slug = { $eq: slug };
+      const entity = await strapi
+        .service("api::konta-klienci-indywidualni.konta-klienci-indywidualni")
+        .find(query);
+      const { results } = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(results[0]);
+    },
+  })
+);
--- src/api/konta-klienci-indywidualni/routes/konta-klienci-indywidualni.js
+++ src/api/konta-klienci-indywidualni/routes/konta-klienci-indywidualni.js
@@ -1,9 +1,11 @@
-'use strict';
+"use strict";
 
 /**
  * konta-klienci-indywidualni router.
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::konta-klienci-indywidualni.konta-klienci-indywidualni');
+module.exports = createCoreRouter(
+  "api::konta-klienci-indywidualni.konta-klienci-indywidualni"
+);
