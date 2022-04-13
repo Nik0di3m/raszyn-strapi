5462476672bde739869c8334ac327852ff0c85e7
Nik0di3m <norbert.waleszczyk@icloud.com>
2022-04-13 09:44:53 +0200

    find one slug

--- src/api/aktualnosci/routes/aktualnosci.js
+++ src/api/aktualnosci/routes/aktualnosci.js
@@ -1,9 +1,24 @@
-'use strict';
+"use strict";
 
 /**
  * aktualnosci router.
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::aktualnosci.aktualnosci');
+module.exports = createCoreRouter(
+  "api::aktualnosci.aktualnosci",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { Slug } = ctx.params;
+      const { query } = ctx;
+
+      const entity = await strapi
+        .service("api::aktualnosci.aktualnosci")
+        .findOne(Slug, query);
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(sanitizedEntity);
+    },
+  })
+);
