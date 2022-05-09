71bd396bd3749325328e5417413fa82cb49b2c0e
Nik0di3m <norbert.waleszczyk@icloud.com>
2022-05-09 21:22:54 +0200

    karty fix

--- src/api/karty-klienci-indywidualni/routes/karty-klienci-indywidualni.js
+++ src/api/karty-klienci-indywidualni/routes/karty-klienci-indywidualni.js
@@ -1,11 +1,11 @@
 "use strict";
 
 /**
- * konta-klienci-indywidualni router.
+ * karty-klienci-indywidualni router.
  */
 
 const { createCoreRouter } = require("@strapi/strapi").factories;
 
 module.exports = createCoreRouter(
-  "api::konta-klienci-indywidualni.konta-klienci-indywidualni"
+  "api::karty-klienci-indywidualni.karty-klienci-indywidualni"
 );
